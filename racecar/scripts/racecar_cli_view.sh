9afb7f9b770aa68ef4455bbf13334a5a14e27145
ding liangxue <dinglx@uincore.net>
2018-05-07 18:59:00 +0800

    Tenda Wi-Fi MW6

--- racecar/scripts/racecar_cli_view.sh
+++ racecar/scripts/racecar_cli_view.sh
@@ -1,6 +1,4 @@
 source ~/catkin_ws/devel/setup.bash
 cd ~/catkin_ws/src/racecar/racecar/scripts
-#source rosenv.sh 192.168.1.98 192.168.1.228
 source rosenv.sh 192.168.5.157 192.168.5.136
-roslaunch racecar teleop_cli.launch
-
+rqt_image_view
