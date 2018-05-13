dac414d870614860434cf7f3df234dad702a830a
dinglx <dinglx@uincore.net>
2018-05-13 15:14:37 +0800

    G29 Shift-N Stop Vehile

--- racecar/scripts/racecar_cli.sh
+++ racecar/scripts/racecar_cli.sh
@@ -1,6 +1,6 @@
 source ~/catkin_ws/devel/setup.bash
 cd ~/catkin_ws/src/racecar/racecar/scripts
 #source rosenv.sh 192.168.1.98 192.168.1.228
-source rosenv.sh 192.168.5.157 192.168.5.136
+source rosenv.sh 192.168.5.158 192.168.5.227
 roslaunch racecar teleop_cli.launch
 
--- racecar/scripts/racecar_cli_test.sh
+++ racecar/scripts/racecar_cli_test.sh
@@ -1,7 +1,7 @@
 source ~/catkin_ws/devel/setup.bash
 cd ~/catkin_ws/src/racecar/racecar/scripts
 #source rosenv.sh 192.168.1.98 192.168.1.228
-source rosenv.sh 192.168.1.225 192.168.1.225
+source rosenv.sh 192.168.1.247 192.168.1.247
 #source rosenv.sh 192.168.5.136 192.168.5.136
 roslaunch racecar teleop_cli.launch
 
